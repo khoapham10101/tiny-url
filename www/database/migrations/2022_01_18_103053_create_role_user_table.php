d4c4f3070f15d8e9da7c8aa4155cf72d322fa55b
Khoa Pham <khoa.pham@fidesio.com>
2022-01-18 17:40:22 +0700

    Second option to create many-to-many relationship

--- www/database/migrations/2022_01_18_103053_create_role_user_table.php
+++ www/database/migrations/2022_01_18_103053_create_role_user_table.php
@@ -15,12 +15,9 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('role_id');
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
-
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
