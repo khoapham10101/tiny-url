2773b76018dbaaa407469ad4be8b77ee2c4010c9
Khoa Pham <khoa.pham@fidesio.com>
2022-01-20 18:41:14 +0700

    Update composer and scripts

--- www/scripts/tasks/migrate.sh
+++ www/scripts/tasks/migrate.sh
@@ -3,11 +3,11 @@
 . $(dirname ${BASH_SOURCE[0]})/../../.env
 . $(dirname ${BASH_SOURCE[0]})/../../.env.local
 
-echo -e "${LIGHT_GREEN}Run Laravel Default Artisan commands.${NC}"
-php artisan package:discover --ansi
-php artisan vendor:publish --tag=laravel-assets --ansi --force
-php -r \"file_exists('.env') || copy('.env.example', '.env');\"
-php artisan key:generate --ansi
+#echo -e "${LIGHT_GREEN}Run Laravel Default Artisan commands.${NC}"
+#php artisan package:discover --ansi
+#php artisan vendor:publish --tag=laravel-assets --ansi --force
+#php -r \"file_exists('.env') || copy('.env.example', '.env');\"
+#php artisan key:generate --ansi
 
 echo -e "${LIGHT_GREEN}Run Laravel Migration.${NC}"
 php artisan migrate
